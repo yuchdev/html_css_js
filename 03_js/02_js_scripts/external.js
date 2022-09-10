2fc9685db5ba9d72df33a35e70d8d1348f61930e
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2022-09-10 11:15:14 +0700

    Intro into JS

---
+++ 03_js/02_js_scripts/external.js
@@ -0,0 +1,9 @@
+// External JS script
+
+// Change the style of <p id="style-change-2">
+function changeStyleExternal() {
+  let p = document.getElementById("style-change-2");
+  p.style.color = "red";
+  p.style.fontFamily = "Arial";
+  p.style.fontSize = "16px";
+}
