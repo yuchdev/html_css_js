7298b553e838d82f2a9f4ec10b9761d18591a80e
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2022-10-13 12:45:07 +0700

    Champer on operators

---
+++ 03_js/02_js_details/02_js_strings/strings.js
@@ -0,0 +1,21 @@
+// Print into HTML element
+function printInElement(element, text) {
+    document.getElementById(element).innerHTML = text;
+}
+
+// Print using document.write()
+function printUsingDocumentWrite(text) {
+    document.write(text);
+}
+
+// Message box with window.alert()
+function printUsingAlert(text) {
+    window.alert(text);
+}
+
+// Execution log with console.log()
+function printUsingConsoleLog(text) {
+    console.log(text);
+}
+
+
